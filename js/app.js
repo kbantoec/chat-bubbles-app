48dcaf7fa7a6e21c27d21fa40427df46501f44a3
Kai Yannick Banto Eckert <y.banto@outlook.com>
2023-09-18 22:08:26 +0200

    Added tail to bubbles, provided them with an adaptive size and added fade out effect to bubbles where the Enter key has not been stroken

--- js/app.js
+++ js/app.js
@@ -5,17 +5,11 @@ let timeOutMilliseconds = 5000;
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        console.log("(Main) -- Cond 1");
-        console.log(bubbles);
         finalizeBubble();
     } else if (event.key.length === 1) {
-        console.log("(Main) -- Cond 2");
-        console.log(bubbles);
         createOrUpdateBubble(event.key);
         resetFadeTimeout();
     } else if (event.key === 'Backspace' && currentBubble) {
-        console.log("(Main) -- Cond 3");
-        console.log(bubbles);
         currentBubble.textContent = currentBubble.textContent.slice(0, -1);
         if (currentBubble.textContent === "") {
             currentBubble.remove();
@@ -23,33 +17,46 @@ document.addEventListener('keydown', function(event) {
         }
         resetFadeTimeout();
     }
-    
-    console.log("(Main) ", currentBubble)
-    console.log("(Main) ", fadeTimeout)
-    console.log("(Main)", bubbles);
 });
 
 function createOrUpdateBubble(text) {
-    if (!currentBubble) {  // if currentBubble is null
+    if (!currentBubble) {  
+        // Create a container for the bubble
+        let container = document.createElement('div');
+        container.classList.add('bubble-container');
+        document.getElementById('chatArea').appendChild(container);
+
+        // Create the bubble inside the container
         currentBubble = document.createElement('div');
         currentBubble.classList.add('bubble');
-        document.getElementById('chatArea').appendChild(currentBubble);
+        container.appendChild(currentBubble);
     }
     currentBubble.textContent += text;
+    // if (!currentBubble) {  // if currentBubble is null
+    //     currentBubble = document.createElement('div');
+    //     currentBubble.classList.add('bubble');
+    //     document.getElementById('chatArea').appendChild(currentBubble);
+    // }
+    // currentBubble.textContent += text;
+
+    const maxLength = 25; // You can adjust this value as needed
+    if (currentBubble.textContent.length > maxLength) {
+        currentBubble.style.width = '80%'; // Set to max width when text length exceeds maxLength
+        currentBubble.style.whiteSpace = 'normal'; // Allow the text to wrap
+    }
 }
 
 function finalizeBubble() {
     if (currentBubble && currentBubble.textContent.trim() !== "") {
-        console.log("(finalizeBubble) -- Cond 1");
-        console.log(currentBubble);
-        console.log(fadeTimeout);
 
         // Create a timeout for the bubble
         let timeoutID = setTimeout(() => {
+            // Get the container of the bubble
+            let container = bubbles[0].element.parentElement;
+            container.classList.add('fadeOut');
             setTimeout(() => {
-                // Remove the bubble's record from the bubbles array
                 bubbles = bubbles.filter(b => b.element !== currentBubble);
-                bubbles[0].element.remove();                
+                container.remove();
                 bubbles[0].element = null;
                 bubbles.shift();
             }, 1000);
@@ -58,36 +65,30 @@ function finalizeBubble() {
         bubbles.push({ element: currentBubble, timeout: timeoutID })
         currentBubble = null;  // Important to make new bubbles
     } else if (currentBubble) {
-        console.log("(finalizeBubble) -- Cond 2");
-        console.log(currentBubble);
-        console.log(fadeTimeout);
         currentBubble.remove();
         currentBubble = null;
     }
 }
 
 function resetFadeTimeout() {
-    console.log("in resetFadeTimeout")
     // Clear the previous timeout
-    if (fadeTimeout) {  // if fadeTimeout is not null
+    if (fadeTimeout) {
         clearTimeout(fadeTimeout);
-        console.log("(resetFadeTimeout) --If 1")
-        console.log(fadeTimeout)
     }
 
     // Set a new timeout if there's a current bubble
     if (currentBubble) {
-        console.log("(resetFadeTimeout) -- If 2 -- Cond 1 -- before");
-        console.log(fadeTimeout);
         fadeTimeout = setTimeout(() => {
-            if (currentBubble) {  // the action to do after time is out
-                currentBubble.remove();
-                currentBubble = null;
+            if (currentBubble) {
+                let container = currentBubble.parentElement; // Get the container of the bubble
+                container.classList.add('fadeOut'); // Add fadeOut class to the container
+
+                // Wait for the fade-out effect to complete before removing the element
+                setTimeout(() => {
+                    container.remove();
+                    currentBubble = null;
+                }, 1000); 
             }
         }, timeOutMilliseconds);
-        console.log("(resetFadeTimeout) -- If 2 -- Cond 1 -- after");
-        console.log(fadeTimeout);
     }
-
-
 }
